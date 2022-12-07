b436d8685eede221335c081e58d220205e50cb00
KevOdo <kev.odas@gmail.com>
2022-12-07 18:20:18 +0100

    Porogramma Calcolatrice

---
+++ Calcolatrice/Calcolatrice/Default.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <table>
+
+            <!-- Display per la calcolatrice -->
+            <tr>
+                <td colspan="4">
+                    <asp:TextBox ID="txtDisplay" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+            <!-- Prima riga di bottoni (1,2,3,+) -->
+            <tr>
+                <td>
+                    <asp:Button ID="btn1" runat="server" Text="1" OnClick="btn1_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn2" runat="server" Text="2" OnClick="btn2_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn3" runat="server" Text="3" OnClick="btn3_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btnPiu" runat="server" Text="+" OnClick="btnPiu_Click" />
+                </td>
+            </tr>
+
+            <!-- Seconda riga di bottoni (4,5,6,-) -->
+            <tr>
+                <td>
+                    <asp:Button ID="btn4" runat="server" Text="4" OnClick="btn4_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn5" runat="server" Text="5" OnClick="btn5_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn6" runat="server" Text="6" OnClick="btn6_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btnMeno" runat="server" Text="-" OnClick="btnMeno_Click" />
+                </td>
+            </tr>
+
+            <!-- Terza riga di bottoni (7,8,9,/) -->
+            <tr>
+                <td>
+                    <asp:Button ID="btn7" runat="server" Text="7" OnClick="btn7_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn8" runat="server" Text="8" OnClick="btn8_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn9" runat="server" Text="9" OnClick="btn9_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btnDiviso" runat="server" Text="/" OnClick="btnDiviso_Click" />
+                </td>
+            </tr>
+
+            <!-- Prima riga di bottoni (C,0,=,*) -->
+            <tr>
+                <td>
+                    <asp:Button ID="btnC" runat="server" Text="C" OnClick="btnC_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btn0" runat="server" Text="0" OnClick="btn0_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btnUguale" runat="server" Text="=" OnClick="btnUguale_Click" />
+                </td>
+
+                <td>
+                    <asp:Button ID="btnPer" runat="server" Text="*" OnClick="btnPer_Click" />
+                </td>
+            </tr>
+
+        </table>
+
+    </form>
+</body>
+</html>
---
+++ Calcolatrice/Calcolatrice/Default.aspx.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    static int n1;
+    static int n2;
+    static string op;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 1 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 1;
+            txtDisplay.Text = txtDisplay.Text + "1";
+        }
+        // se c'e' l'operatore aggiungi 1 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 1;
+            txtDisplay.Text = txtDisplay.Text + "1";
+        }
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 2 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 2;
+            txtDisplay.Text = txtDisplay.Text + "2";
+        }
+        // se c'e' l'operatore aggiungi 2 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 2;
+            txtDisplay.Text = txtDisplay.Text + "2";
+        }
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 3 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 3;
+            txtDisplay.Text = txtDisplay.Text + "3";
+        }
+        // se c'e' l'operatore aggiungi 3 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 3;
+            txtDisplay.Text = txtDisplay.Text + "3";
+        }
+    }
+
+    protected void btn4_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 4 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 4;
+            txtDisplay.Text = txtDisplay.Text + "4";
+        }
+        // se c'e' l'operatore aggiungi 4 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 4;
+            txtDisplay.Text = txtDisplay.Text + "4";
+        }
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 5 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 5;
+            txtDisplay.Text = txtDisplay.Text + "5";
+        }
+        // se c'e' l'operatore aggiungi 5 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 5;
+            txtDisplay.Text = txtDisplay.Text + "5";
+        }
+    }
+
+    protected void btn6_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 6 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 6;
+            txtDisplay.Text = txtDisplay.Text + "6";
+        }
+        // se c'e' l'operatore aggiungi 6 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 6;
+            txtDisplay.Text = txtDisplay.Text + "6";
+        }
+    }
+
+    protected void btn7_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 7 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 7;
+            txtDisplay.Text = txtDisplay.Text + "7";
+        }
+        // se c'e' l'operatore aggiungi 7 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 7;
+            txtDisplay.Text = txtDisplay.Text + "7";
+        }
+    }
+
+    protected void btn8_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 8 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 8;
+            txtDisplay.Text = txtDisplay.Text + "8";
+        }
+        // se c'e' l'operatore aggiungi 8 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 8;
+            txtDisplay.Text = txtDisplay.Text + "8";
+        }
+    }
+
+    protected void btn9_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 9 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = (n1 * 10) + 9;
+            txtDisplay.Text = txtDisplay.Text + "9";
+        }
+        // se c'e' l'operatore aggiungi 9 al secondo numero
+        else
+        {
+            n2 = (n2 * 10) + 9;
+            txtDisplay.Text = txtDisplay.Text + "9";
+        }
+    }
+
+    protected void btn0_Click(object sender, EventArgs e)
+    {
+        // se non c'e' l'operatore aggiungi 0 al primo numero
+        if (String.IsNullOrEmpty(op))
+        {
+            n1 = n1 + 0;
+            txtDisplay.Text = txtDisplay.Text + "0";
+        }
+        // se c'e' l'operatore aggiungi 0 al secondo numero
+        else
+        {
+            n2 = n2 + 0;
+            txtDisplay.Text = txtDisplay.Text + "0";
+        }
+    }
+
+    protected void btnPiu_Click(object sender, EventArgs e)
+    {
+        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        {
+            op = "+";
+            txtDisplay.Text = "";
+        }
+    }
+
+    protected void btnMeno_Click(object sender, EventArgs e)
+    {
+        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        {
+            op = "-";
+            txtDisplay.Text = "";
+        }
+    }
+
+    protected void btnDiviso_Click(object sender, EventArgs e)
+    {
+        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        {
+            op = "/";
+            txtDisplay.Text = "";
+        }
+    }
+
+    protected void btnPer_Click(object sender, EventArgs e)
+    {
+        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        {
+            op = "*";
+            txtDisplay.Text = "";
+        }
+    }
+
+    protected void btnUguale_Click(object sender, EventArgs e)
+    {
+        if (op == "+" && n2 != 0)
+        {
+            txtDisplay.Text = (n1 + n2).ToString();
+        } else if (op == "-" && n2 != 0)
+        {
+            txtDisplay.Text = (n1 - n2).ToString();
+        }
+        else if (op == "*" && n2 != 0)
+        {
+            txtDisplay.Text = (n1 * n2).ToString();
+        }
+        else if (op == "/" && n2 != 0)
+        {
+            txtDisplay.Text = (n1 / n2).ToString();
+        }
+    }
+
+    protected void btnC_Click(object sender, EventArgs e)
+    {
+        n1 = 0;
+        n2 = 0;
+        op = "";
+        txtDisplay.Text = "";
+    }
+
+}
