dda43cbe6b03a9a15075af8da98551b424601f6f
KevOdo <kev.odas@gmail.com>
2022-12-09 17:17:44 +0100

    Null check e cambio tipo variabili

--- Calcolatrice/Calcolatrice/Default.aspx.cs
+++ Calcolatrice/Calcolatrice/Default.aspx.cs
@@ -10,8 +10,8 @@ using System.Web.UI.WebControls;
 public partial class _Default : System.Web.UI.Page
 {
     // impposta variabili iniziali
-    static int n1;
-    static int n2;
+    static int? n1;
+    static int? n2;
     static string op;
 
     protected void Page_Load(object sender, EventArgs e)
@@ -19,17 +19,28 @@ public partial class _Default : System.Web.UI.Page
 
     }
 
+    public int? checkNull(int? num)
+    {
+        if (num == null)
+        {
+            num = 0;
+        }
+        return num;
+    }
+
     protected void btn1_Click(object sender, EventArgs e)
     {
         // se non c'e' l'operatore concatena 1 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 1;
             txtDisplay.Text = txtDisplay.Text + "1";
         }
         // se c'e' l'operatore concatena 1 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 1;
             txtDisplay.Text = txtDisplay.Text + "1";
         }
@@ -40,12 +51,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 2 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 2;
             txtDisplay.Text = txtDisplay.Text + "2";
         }
         // se c'e' l'operatore concatena 2 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 2;
             txtDisplay.Text = txtDisplay.Text + "2";
         }
@@ -56,12 +69,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 3 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 3;
             txtDisplay.Text = txtDisplay.Text + "3";
         }
         // se c'e' l'operatore concatena 3 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 3;
             txtDisplay.Text = txtDisplay.Text + "3";
         }
@@ -72,12 +87,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 4 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 4;
             txtDisplay.Text = txtDisplay.Text + "4";
         }
         // se c'e' l'operatore concatena 4 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 4;
             txtDisplay.Text = txtDisplay.Text + "4";
         }
@@ -88,12 +105,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 5 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 5;
             txtDisplay.Text = txtDisplay.Text + "5";
         }
         // se c'e' l'operatore concatena 5 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 5;
             txtDisplay.Text = txtDisplay.Text + "5";
         }
@@ -104,12 +123,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 6 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 6;
             txtDisplay.Text = txtDisplay.Text + "6";
         }
         // se c'e' l'operatore concatena 6 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 6;
             txtDisplay.Text = txtDisplay.Text + "6";
         }
@@ -120,12 +141,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 7 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 7;
             txtDisplay.Text = txtDisplay.Text + "7";
         }
         // se c'e' l'operatore concatena 7 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 7;
             txtDisplay.Text = txtDisplay.Text + "7";
         }
@@ -136,12 +159,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 8 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 8;
             txtDisplay.Text = txtDisplay.Text + "8";
         }
         // se c'e' l'operatore concatena 8 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 8;
             txtDisplay.Text = txtDisplay.Text + "8";
         }
@@ -152,12 +177,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 9 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = (n1 * 10) + 9;
             txtDisplay.Text = txtDisplay.Text + "9";
         }
         // se c'e' l'operatore concatena 9 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = (n2 * 10) + 9;
             txtDisplay.Text = txtDisplay.Text + "9";
         }
@@ -168,12 +195,14 @@ public partial class _Default : System.Web.UI.Page
         // se non c'e' l'operatore concatena 0 al primo numero
         if (String.IsNullOrEmpty(op))
         {
+            n1 = checkNull(n1);
             n1 = n1 + 0;
             txtDisplay.Text = txtDisplay.Text + "0";
         }
         // se c'e' l'operatore concatena 0 al secondo numero
         else
         {
+            n2 = checkNull(n2);
             n2 = n2 + 0;
             txtDisplay.Text = txtDisplay.Text + "0";
         }
@@ -182,7 +211,7 @@ public partial class _Default : System.Web.UI.Page
     protected void btnPiu_Click(object sender, EventArgs e)
     {
         // se non c'e' l'operatore, ed esiste il primo numero ma non il secondo, imposta l'operatore come +
-        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        if (String.IsNullOrEmpty(op) && n1 != null && n2 == null)
         {
             op = "+";
             txtDisplay.Text = "";
@@ -192,7 +221,7 @@ public partial class _Default : System.Web.UI.Page
     protected void btnMeno_Click(object sender, EventArgs e)
     {
         // se non c'e' l'operatore, ed esiste il primo numero ma non il secondo, imposta l'operatore come -
-        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        if (String.IsNullOrEmpty(op) && n1 != null && n2 == null)
         {
             op = "-";
             txtDisplay.Text = "";
@@ -202,7 +231,7 @@ public partial class _Default : System.Web.UI.Page
     protected void btnDiviso_Click(object sender, EventArgs e)
     {
         // se non c'e' l'operatore, ed esiste il primo numero ma non il secondo, imposta l'operatore come /
-        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        if (String.IsNullOrEmpty(op) && n1 != null && n2 == null)
         {
             op = "/";
             txtDisplay.Text = "";
@@ -212,7 +241,7 @@ public partial class _Default : System.Web.UI.Page
     protected void btnPer_Click(object sender, EventArgs e)
     {
         // se non c'e' l'operatore, ed esiste il primo numero ma non il secondo, imposta l'operatore come *
-        if (String.IsNullOrEmpty(op) && n1 != 0 && n2 == 0)
+        if (String.IsNullOrEmpty(op) && n1 != null && n2 == null)
         {
             op = "*";
             txtDisplay.Text = "";
@@ -222,34 +251,39 @@ public partial class _Default : System.Web.UI.Page
     protected void btnUguale_Click(object sender, EventArgs e)
     {
         // se c'e' l'operatore ed il secondo numero, esegui il calcolo ADDIZIONE
-        if (op == "+" && n2 != 0)
+        if (op == "+" && n2 != null)
         {
             txtDisplay.Text = (n1 + n2).ToString();
         }
         // se c'e' l'operatore ed il secondo numero, esegui il calcolo SOTTRAZIONE
-        else if (op == "-" && n2 != 0)
+        else if (op == "-" && n2 != null)
         {
             txtDisplay.Text = (n1 - n2).ToString();
         }
         // se c'e' l'operatore ed il secondo numero, esegui il calcolo MOLTIPLICAZIONE
-        else if (op == "*" && n2 != 0)
+        else if (op == "*" && n2 != null)
         {
             txtDisplay.Text = (n1 * n2).ToString();
         }
         // se c'e' l'operatore ed il secondo numero, esegui il calcolo DIVISIONE
-        else if (op == "/" && n2 != 0)
+        else if (op == "/" && n2 != null)
         {
             txtDisplay.Text = (n1 / n2).ToString();
+        }
+        else if (String.IsNullOrEmpty(op) && n1 != null && n2 == null)
+        {
+            txtDisplay.Text = n1.ToString();
         }
     }
 
     // cancella tutto il contenuto del display e dati salvati
     protected void btnC_Click(object sender, EventArgs e)
     {
-        n1 = 0;
-        n2 = 0;
         op = "";
         txtDisplay.Text = "";
+        n1 = null;
+        n2 = null;
+
     }
 
 }
