06747c12686547742eeeb1a5999529efa4710e1b
KevOdo <43936466+KevOdo@users.noreply.github.com>
2023-02-01 14:30:56 +0100

    TEMPLATE

---
+++ TEMPLATE/TEMPLATE/Main.master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    
+    }
+
+}
---
+++ TEMPLATE/TEMPLATE/scripts/main.js
@@ -0,0 +1,23 @@
+﻿function toggle(id) {
+    const elem = document.getElementById(id)
+    if (elem.style.display == "block") {
+        elem.style.display = "none";
+        elem.style.height = "0";
+    } else {
+        elem.style.display = "block";
+        elem.style.height = "100%";
+    }
+}
+
+function toggleSide() {
+    const elem = document.getElementById('scroll');
+    if (elem.style.width == "0px") {
+        elem.style.width = "250px";
+    } else {
+        elem.style.width = "0px";
+    }
+}
+
+function changeBurger(x) {
+    x.classList.toggle("change");
+}
